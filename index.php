e4a4c4dcedee5c47395c39975fc6cdae7bf83ce8
Franziska Hinkelmann <fhinkel@vt.edu>
2010-08-12 10:06:57 -0600

    more stuff to the helloWorld php script

--- index.php
+++ index.php
@@ -6,5 +6,24 @@
 <?php
 echo "Hello World!";
 ?>
+<?php
+$M2command = $_POST["command"];
+if (!isset($_POST['submit'])) { // if page is not submitted to itself echo the form
+  echo "not submitted yet<br>";
+
+?>
+<form action="<?php echo $PHP_SELF;?>" method="post">
+
+	Enter M2 command: <br> 
+	<input type="text" name="command" /><br />
+<input type="submit" value="submit" name="submit" />
+</form>
+<?
+} else {
+  echo "<br>hello, you entered ".$M2command. ".<br>";
+  $my_s = "M2 -e '". $M2command. "; exit 0'";
+  echo `$my_s`;
+}?>
+
 </body>
 </html>

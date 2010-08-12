1579196b7526bfb2bda5128b619d7944b57628a8
Franziska Hinkelmann <fhinkel@vt.edu>
2010-08-11 21:10:34 -0600

    initial working tracer bullet

--- index.pl
+++ index.pl
@@ -11,17 +11,20 @@ use CGI qw( :standard );
 print header, start_html( -title=>'TryM2');
 print start_multipart_form(-name=>'form1', -method =>"POST", -onSubmit=>"return validate()");
 
-print "Enter M2 command";
-print textfield(-name=>'command', -size=>'10', -maxlength=>2, -default=>'print version');
+print "Enter M2 command: ";
+print textfield(-name=>'command', -size=>'16', -default=>'print version');
 
 print "<center>", submit('button_name','Submit'),"</center><br><br>";
 
-print end_form;
 
 $command = param('command');
+$button = param('button_name');
 
-if ($button_name eq "Submit") {
-  print `ruby runner.rb $command`;
+if ($button eq "Submit") {
+  print "Answer: <br>";
+  print `ruby runner.rb "$command"`;
+  print "<br>";
 }
 
+print end_form;
 
--- runner.rb
+++ runner.rb
@@ -6,10 +6,10 @@
 # Takes input from dvd website and passes it to M2 to compute fixed points
 # returns 0 (no errors) or 1 (errors) 
 
-puts "Hello Mike"
 
 unless ARGV.size == 1
-  puts "Usage: ruby runner.rb command"
+  puts "Usage: ruby runner.rb command<br>"
+  puts ARGV.size 
   exit 0
 end
 
@@ -17,11 +17,13 @@ end
 command = ARGV[0]
 
 
-puts  "M2 --stop --no-debug --silent -q -e '#{command}; exit 0'"
+puts "This is the command we'll run<br>"
+puts  "M2 --stop --no-debug --silent -q -e '#{command}; exit 0'<br>"
 result = `M2 --stop --no-debug --silent -q -e '#{command}; exit 0'`
 
 
 puts result
+puts "<br>"
 
 
 exit 0

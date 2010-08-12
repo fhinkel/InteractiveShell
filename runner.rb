38fc64153f849645d36a31c7695b7b331a7357f4
Franziska Hinkelmann <fhinkel@vt.edu>
2010-08-11 20:56:26 -0600

    simple runner script to exectue M2

---
+++ runner.rb
@@ -0,0 +1,23 @@
+# ruby runner.rb command
+
+## Mike, Greg, Franziska Hinkelmann 
+
+
+# Takes input from dvd website and passes it to M2 to compute fixed points
+# returns 0 (no errors) or 1 (errors) 
+
+unless ARGV.size == 1
+  puts "Usage: ruby runner.rb command"
+  exit 0
+end
+
+command = ARGV[0]
+
+result = `M2 --stop --no-debug --silent -q -e '#{command}; exit0'`
+
+
+puts results
+
+
+exit 0
+

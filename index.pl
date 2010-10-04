13162655dc7064f13d28ddfe7132d9029731e03e
Franziska Hinkelmann <fhinkel@vt.edu>
2010-10-04 08:00:25 -0700

    html table to have input left, output on the right

--- index.pl
+++ index.pl
@@ -11,11 +11,13 @@ use CGI qw( :standard );
 print header, start_html( -title=>'TryM2');
 print start_multipart_form(-name=>'form1', -method =>"POST", -onSubmit=>"return validate()");
 
+print "<table><tr> <td valign=top>";
 print "Enter M2 command: <br>";
 print textarea(-name=>'command', -rows=>'16', -columns=>50, -default=>'version');
 
 print "<center>", submit('button_name','Submit'),"</center><br><br>";
 
+print "</td><td>";
 
 $command = param('command');
 $button = param('button_name');
@@ -25,6 +27,8 @@ if ($button eq "Submit") {
   print "Answer: $ans <br>";
   print "<br>";
 }
+
+print "</td></tr></table>";
 
 print end_form;
 

ea60fdafcb8b49ca806af47a547f72a1880f7a8c
Lars Kastner <lars@t500.(none)>
2010-10-06 22:38:38 +0200

    Added some comments.

--- sockets/M2Client.php
+++ sockets/M2Client.php
@@ -21,20 +21,30 @@ echo $cmd;
 // $_SESSION['cmds']=$_SESSION['cmds'].$cmd." ";
 
 echo "<br> Id: ".$id."<br><br>\n";
+
+// Sending id to socket server for connecting to correct M2 process.
 socket_write($socket, $id."\n", strlen($id."\n"));
 echo "wrote id\n";
 
 socket_write($socket, $cmd."\n", strlen($cmd."\n"));
+// Sending id second time for indicating end of input.
 socket_write($socket, $id."\n", strlen($id."\n"));
 socket_close($socket);
 while(!file_exists("results_".$id.".txt")){sleep(1);}
 //echo "file exists";
+
+// Checking if Macaulay is done. If everything went well, the
+// last line of the output usually looks like "i** :", so
+// we cut of the trailing spaces and check whether the last
+// character is ":".
 $output = strrev(trim(file_get_contents("results_".$id.".txt")));
 //echo $output;
 while($output[0] != ":"){
 	sleep(1);
 	$output = strrev(trim(file_get_contents("results_".$id.".txt")));
 }
+
+// Reformat output for html use.
 $output = str_replace("\n", "<br>\n", file_get_contents("results_".$id.".txt"));
 echo $output."<br>\n";
 ?>
--- sockets/M2Server.php
+++ sockets/M2Server.php
@@ -18,12 +18,22 @@ socket_listen($sock, 5);
 
 do {
 	echo "waiting\n";
+	// This will wait for a new incoming connection. The
+	// loop is not executed without new connection. This means
+	// that no M2pipe is killed without a new connection even if
+	// the timeout is hit. To avoid this behaviour one can set
+	// socket_set_nonblock() before loop.
 	$msgsock = socket_accept($sock);
 	$id = trim(socket_read($msgsock,2048, PHP_NORMAL_READ));
 	//echo $id."\n";
 	//echo "id end\n";
 	// Checking if there is a process for this id:
 	if(!isset($pipe[$id])){
+	
+	    // If there is no pipe we might have to get the earlier
+		// commands from the client. One can send a response to
+		// client for indicating this. Since they can be saved
+		// in the session object this should be easy.
 		$pipe[$id] = popen("M2 > results_".$id.".txt 2>&1",'w');
 	}
 	// Setting time limit:
@@ -49,9 +59,11 @@ do {
 	// Closing pipes:
 	foreach($remove as $key => $t){
 		echo "Closing: ".$key."\n";
+		// closing pipes.
 		pclose($pipe[$key]);
 		unset($connections[$key]);
 		unset($pipe[$key]);
+		// remove output files.
 		unlink("results_".$key.".txt");
 	}
 	echo count($remove)."\n";

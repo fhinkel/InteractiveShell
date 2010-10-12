eeb8407bbbeb11c44b8b1fdd5dc66606c1e6e847
Franziska Hinkelmann <fhinkel@vt.edu>
2010-10-12 14:43:17 -0400

    trying to call php from mikes file

--- m2-mike.js
+++ m2-mike.js
@@ -1,125 +1,63 @@
 $(document).ready(function() {
-    // add markup to container and apply click handlers to anchors
-    //$("#header a").click(function(e) {
-    $('#M2In').keypress(function(e){
-	if(e.which == 13){
-	  $('#M2Out').append("Bravo, you just hit enter!\n");
-	  $('#M2Out').append( getSelected());
-	}
-      });
-
-    $("#button1").click(function(e) {
-	$("#M2Out").append($("#M2In").val());
-	$("#M2Out").append("\n");
-	$("#M2Out").append(window.getSelected());
-	$("#M2Out").append("\n");
-      });
-
-    $("#submit").click(function(e) {
-        // stop normal link click
-        e.preventDefault();
-	
-        //$("#M2Out").append($(this).html());
-	$("#M2Out").append($("#M2In").val());
-	$("#M2Out").append("\n");
-	$('#M2Out').append( getSelected());
-	//$('#M2Out').append( document.selection.createRange().text);
-	
-	
-        // send request
-        // $.post("rate.php", {rating: $(this).html()}, function(xml) {
-        // format and output result
-        // $("#rating").html(
-        // "Thanks for rating, current average: " +
-        //$("average", xml).text() +
-        //", number of votes: " +
-        //$("count", xml).text()
-        //);
-      });
+  // add markup to container and apply click handlers to anchors
+  //$("#header a").click(function(e) {
+  $('#M2In').keypress(function(e) {
+    if(e.which == 13){
+      $('#M2Out').append("Bravo, you just hit enter!\n");
+      $('#M2Out').append( getSelected());
+    }
   });
 
+  $("#button1").click(function(e) {
+    myCommand = $("#M2In").val()
+    $("#M2Out").append( myCommand);
+    $("#M2Out").append("\n");
+    $("#M2Out").append("Button clicked! Send to PHP script:\n");
 
+    // call to php script
+    $.post("sockets/M2Client.php", {cmd: myCommand}, function(data){
+      if(data != "0") {
+        $("#container").html("<b>Session initialized successfully!"+ data + "</b>");
+        $("#cmd").val("");
+      } else {
+        $("#container").html("<b>Something Broke! HELP!</b>");
+      }
+    });
+  });
+
+  $("#M2Out").append(window.getSelected());
+  $("#M2Out").append("\n");
+});
 
 
 /* attempt to find a text selection */
 function getSelected() {
-	if(window.getSelection) { $('#M2Out').append("option1\n --" + window.getSelection() + "--"); return window.getSelection();  }
-	
-	else if(document.getSelection) { $('#M2Out').append("option2");return document.getSelection(); }
-	else {
-		var selection = document.selection && document.selection.createRange();
-		if(selection.text) { $('#M2Out').append("option3"); return selection.text; }
-		$('#M2Out').append("option4");
-		return false;
-	}
-	$('#M2Out').append("option never");
-	return false;
+  if(window.getSelection) { $('#M2Out').append("Get Selected! option1\n --" + window.getSelection() + "--"); return window.getSelection();  }
+
+  else if(document.getSelection) { $('#M2Out').append("option2");return document.getSelection(); }
+  else {
+    var selection = document.selection && document.selection.createRange();
+    if(selection.text) { $('#M2Out').append("option3"); return selection.text; }
+    $('#M2Out').append("option4");
+    return false;
+  }
+  $('#M2Out').append("option never");
+  return false;
 }
 /* create sniffer */
 /*$(document).ready(function() {
-	$('#content-area').mouseup(function() {
-		var selection = getSelected();
-		if(selection && (selection = new String(selection).replace(/^\s+|\s+$/g,''))) {
-			$.ajax({
-				type: 'post',
-				url: 'ajax-selection-copy.php',
-				data: 'selection=' + encodeURI(selection)
-			});
-		}
-	});
+  $('#content-area').mouseup(function() {
+  var selection = getSelected();
+  if(selection && (selection = new String(selection).replace(/^\s+|\s+$/g,''))) {
+  $.ajax({
+type: 'post',
+url: 'ajax-selection-copy.php',
+data: 'selection=' + encodeURI(selection)
+});
+}
+});
 });*/
 
 
-$(document).ready(function() {
-    // generate markup
-    //$("#header").append("Please rate: ");
-
-    //for (var i = 1; i <= 5; i++)
-    //$("#header").append("<a href='#'>" + i + "</a> ");
-
-
-
-    // add markup to container and apply click handlers to anchors
-    //$("#header a").click(function(e) {
-		$('#M2In').keypress(function(e){
-		      if(e.which == 13){
-		       $('#M2Out').append("Bravo, you just hit enter!\n");
-				$('#M2Out').append( getSelected());
-		       }
-		      });
-	
-	$("#submit").click(function(e) {
-        // stop normal link click
-        e.preventDefault();
-
-        //$("#M2Out").append($(this).html());
- 		$("#M2Out").append($("#M2In").val());
-		$("#M2Out").append("\n");
-		$('#M2Out').append( getSelected());
-		//$('#M2Out').append( document.selection.createRange().text);
-
-
-        // send request
-        // $.post("rate.php", {rating: $(this).html()}, function(xml) {
-        // format and output result
-        // $("#rating").html(
-        // "Thanks for rating, current average: " +
-        //$("average", xml).text() +
-        //", number of votes: " +
-        //$("count", xml).text()
-        //);
-    });
-});
-
-// Get user selection text on page
-/*function getSelectedText() {
-    if (window.getSelection) {
-        return window.getSelection();
-    }
-    else if (document.selection) {
-        return document.selection.createRange().text;
-    }
-    return '';
-}*/
 
 

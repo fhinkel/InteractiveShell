8b27ec1b1b8c3c60bf814c688f3a9beb56939cc7
Franziska Hinkelmann <fhinkel@vt.edu>
2010-12-08 11:43:04 -0500

    send commands on shift-enter

--- m2.js
+++ m2.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-    $('#M2In').keypress(function(e){
-	if(e.which == 13){
-	  sendToM2( getSelected(), "You hit enter!! ");	  
-	}
-      });
+    $('#M2In').keypress(function(e) {
+        if (e.which == 13 && e.shiftKey) {
+			e.preventDefault(); // do not make a line break or remove selected text when sending
+            sendToM2(getSelected(), "You hit shift-enter!! ");
+        }
+    });
 
     $("#send").click(sendCallback);
     $("#reset").click(resetCallback);
@@ -11,60 +12,64 @@ $(document).ready(function() {
 });
 
 function resetCallback(e) {
-  if (!sendToM2( ">>RESET<<", "We are resetting the current M2 session.\n")) {
-    $("#M2Out").val($("#M2Out").val() + "<b>Something Broke! HELP!</b>");
-  }
-  $("#M2Out").val("");
+    if (!sendToM2(">>RESET<<", "We are resetting the current M2 session.\n")) {
+        $("#M2Out").val($("#M2Out").val() + "<b>Something Broke! HELP!</b>");
+    }
+    $("#M2Out").val("");
 }
 
 function sendCallback(e) {
-  var str = getSelected();
-  sendToM2( str, "");
-  return false;
+    var str = getSelected();
+    sendToM2(str, "");
+    return false;
 }
 
 function seeCallback(e) {
-  var str = getSelected();
-  alert(str);
-  return false;
+    var str = getSelected();
+    alert(str);
+    return false;
 }
 
 // return false on error
-function sendToM2( myCommand, baseString ) {
-  $.post("sockets/M2Client.php", {cmd: myCommand}, function(data){
-      if(data != "0") {
-	$("#M2Out").val(baseString + data );
-	scrollDown();
-      } else {
-	$("#M2Out").val($("#M2Out").val() + "Something Broke! HELP!");
-	return false;
-      }
+function sendToM2(myCommand, baseString) {
+    $.post("sockets/M2Client.php", {
+        cmd: myCommand
+    },
+    function(data) {
+        if (data != "0") {
+            $("#M2Out").val(baseString + data);
+            scrollDown();
+        } else {
+            $("#M2Out").val($("#M2Out").val() + "Something Broke! HELP!");
+            return false;
+        }
     });
-  return true;
+    return true;
 }
 
-function scrollDown() { 
-  mySize = $('#M2Out').val().length;
-  $('#M2Out').scrollTop(mySize);
-  return false; // Return false to cancel the default link action
+function scrollDown() {
+    mySize = $('#M2Out').val().length;
+    $('#M2Out').scrollTop(mySize);
+    return false;
+    // Return false to cancel the default link action
 }
 
 /* get selected text, or current line, in the textarea #M2In */
 function getSelected() {
-  var str = $("#M2In").val();
-  var start = $("#M2In")[0].selectionStart;
-  var end = $("#M2In")[0].selectionEnd;
-  if (start == end) {
-    // grab the current line
-    start = 1 + str.lastIndexOf("\n", end-1);
-    var endPos = str.indexOf("\n", start);
-    if ( endPos != -1) {
-      end = endPos;
-    } else {
-      end = str.length;
+    var str = $("#M2In").val();
+    var start = $("#M2In")[0].selectionStart;
+    var end = $("#M2In")[0].selectionEnd;
+    if (start == end) {
+        // grab the current line
+        start = 1 + str.lastIndexOf("\n", end - 1);
+        var endPos = str.indexOf("\n", start);
+        if (endPos != -1) {
+            end = endPos;
+        } else {
+            end = str.length;
+        }
     }
-  }
-  return str.slice(start,end) + "\n";  
+    return str.slice(start, end) + "\n";
 }
 
 /* Info on selected text:

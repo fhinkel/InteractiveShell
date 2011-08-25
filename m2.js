a2dc83508030d9da7ce468eee209386c1e8e7929
Mike Stillman <mikestillman1@gmail.com>
2011-08-25 12:38:31 -0400

    fixed lesson navigation

--- m2.js
+++ m2.js
@@ -12,12 +12,6 @@ var timerobject;
 $(document).ready(function() {
     checkForNewData(offset);
 
-	$('[lessonid]').live('click', function(){
-		var lessonId = $(this).attr('lessonid');
-		lessonNr = parseInt( lessonId.match(/\d/g ));
-		//alert("Lesson " + lessonNr);
-		loadLesson(lessonNr);
-	});
 	
 	$('#M2In').keypress(sendOnEnterCallback('#M2In'));
     $("#send").click(sendCallback( '#M2In' ));
@@ -88,13 +82,20 @@ function createMenu()
 	var i = 1;
 	$("#tutorial h4").each( function() {
 		var title = $(this).text();
-		//alert( title );
+		alert("<li class='arrow'><a href='#M2' lessonid='lesson" + i +"'>Lesson " + i +": " + title + "</a></li>" );
 		$("#menu").append(
 			"<li class='arrow'><a href='#M2' lessonid='lesson" + i +"'>Lesson " + i +": " + title + "</a></li>");
 		// now create new menu item 
 		//$('[lessonId]').text( "Lesson " + i + ": " + title );
 		i = i + 1;
 	} );
+
+	$('[lessonid]').click(function(){
+		var lessonId = $(this).attr('lessonid');
+		lessonNr = parseInt( lessonId.match(/\d/g ));
+		//alert("Lesson " + lessonNr);
+		loadLesson(lessonNr);
+	});
 
 }
 	

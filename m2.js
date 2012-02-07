4d0b6425c1c2bebf017b16650e6ed0be272ef18c
Mike Stillman <mikestillman1@gmail.com>
2012-02-07 14:45:03 -0500

    adding mb extruder code

--- m2.js
+++ m2.js
@@ -44,6 +44,7 @@ $(document).ready(function() {
     $("#send").hide();
     $("#pageIndex").hide();
     
+
 	$("#tutorial").load("tutorial.html", function () {
  		maxLesson = $('.lesson').children().length;
 		//createMenu();
@@ -79,6 +80,16 @@ $(document).ready(function() {
         });
     });
     
+$(function(){
+    $("#extruderBottom").buildMbExtruder({
+                position:"bottom",
+                width:350,
+                extruderOpacity:1,
+                onExtOpen:function(){},
+                onExtContentLoad:function(){},
+                onExtClose:function(){}
+            });
+    });
 
     
     

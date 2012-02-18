46c9756cfb2924c95732aca7661581524458248d
Mike Stillman <mikestillman1@gmail.com>
2012-02-18 10:06:07 -0500

    changed tabs to 4 spaces

--- m2.js
+++ m2.js
@@ -1,74 +1,74 @@
 var trym2 = {
-	offset: 0,
-	waitingtime: 2500, // in ms.  Each time we poll for data and don't receive it, we wait longer.
-	maxwaitingtime: 60*1000*10, // 10 minutes
-	minwaitingtime: 100,
-	lessonNr: 1,
-	maxLesson: 1,
-	timerobject: 0
+    offset: 0,
+    waitingtime: 2500, // in ms.  Each time we poll for data and don't receive it, we wait longer.
+    maxwaitingtime: 60*1000*10, // 10 minutes
+    minwaitingtime: 100,
+    lessonNr: 1,
+    maxLesson: 1,
+    timerobject: 0
 }
 
 
 $(document).ready(function() {
-	
-	$('.submenuItem').live("click", function(){
-		console.log( "You clicked a submenuItem: " + $(this).html() );
-		var lessonId = $(this).attr('lessonid');
-		trym2.lessonNr = parseInt( lessonId.match(/\d/g ));
-		$("#tutorial").html( $("#menuTutorial").html() );
-		var i = 1;
-		$("#tutorial h4").each( function() {
-				$(this).parent().attr('lessonid', i); // add an ID to every lesson div
-			i = i + 1;
-		} );
-		trym2.maxLesson = $('#tutorial .lesson').children().length;
-		loadLesson(trym2.lessonNr);
-	});
-		
-	
-	$('#help-dialog').dialog({
-			height: 340,
-			width: 460,
-			modal: true,
-			autoOpen: false
-		});
-	$('#help').click(helpScreen);
-	
-	
+    
+    $('.submenuItem').live("click", function(){
+        console.log( "You clicked a submenuItem: " + $(this).html() );
+        var lessonId = $(this).attr('lessonid');
+        trym2.lessonNr = parseInt( lessonId.match(/\d/g ));
+        $("#tutorial").html( $("#menuTutorial").html() );
+        var i = 1;
+        $("#tutorial h4").each( function() {
+                $(this).parent().attr('lessonid', i); // add an ID to every lesson div
+            i = i + 1;
+        } );
+        trym2.maxLesson = $('#tutorial .lesson').children().length;
+        loadLesson(trym2.lessonNr);
+    });
+        
+    
+    $('#help-dialog').dialog({
+            height: 340,
+            width: 460,
+            modal: true,
+            autoOpen: false
+        });
+    $('#help').click(helpScreen);
+    
+    
     SyntaxHighlighter.all();
-		
+        
     checkForNewData(trym2.offset);
 
-	$('#M2In').keypress(sendOnEnterCallback('#M2In'));
+    $('#M2In').keypress(sendOnEnterCallback('#M2In'));
     $("#send").click(sendCallback( '#M2In' ));
     $("#reset").click(resetCallback);
 
     $("code").live("click", function() { 
        $(this).effect("highlight", {color: 'red'}, 800);
-	    var code = $(this).html();
-		$("#M2In").val($("#M2In").val() + "\n" + code);
-		scrollDown( "#M2In" );
-		sendToM2(">>SENDCOMMANDS<<\n" + code);
-	});
+        var code = $(this).html();
+        $("#M2In").val($("#M2In").val() + "\n" + code);
+        scrollDown( "#M2In" );
+        sendToM2(">>SENDCOMMANDS<<\n" + code);
+    });
 
     $("#inputarea").hide();
     $("#send").hide();
     $("#pageIndex").hide();
     
-	$("#tutorial").html("<div class='lesson' lessonid='1'><div><br>Get started by <b>selecting a tutorial</b> from the menu on the upper right corner or by using the Macaulay2 console. Have fun!</div></div>");
-	loadLesson(trym2.lessonNr);
-	trym2.maxLesson = $('.lesson').children().length;
-	
-	
-	$("#next").click( function(){
-	    switchLesson(1);
-	});
-	$("#previous").click( function(){
-	    switchLesson(-1);
-	});
+    $("#tutorial").html("<div class='lesson' lessonid='1'><div><br>Get started by <b>selecting a tutorial</b> from the menu on the upper right corner or by using the Macaulay2 console. Have fun!</div></div>");
+    loadLesson(trym2.lessonNr);
+    trym2.maxLesson = $('.lesson').children().length;
+    
+    
+    $("#next").click( function(){
+        switchLesson(1);
+    });
+    $("#previous").click( function(){
+        switchLesson(-1);
+    });
 
-	// swipe changed to swipeXXX to remove functionality for testing
-	$(function(){ $("#leftwindow").bind("swipeXXX",function(event, info) {
+    // swipe changed to swipeXXX to remove functionality for testing
+    $(function(){ $("#leftwindow").bind("swipeXXX",function(event, info) {
         if (info.direction === "left"){
             switchLesson(1);
         } else if (info.direction === "right") {
@@ -79,23 +79,23 @@ $(document).ready(function() {
         });
     });
     
-	$(function(){   	 
-		$("#extruderTop").buildMbExtruder({
-	                position:"top",
-	                width:350,
-	                extruderOpacity:1,
-	                onExtOpen:function(){},
-	                onExtContentLoad:function(){},
-	                onExtClose:function(){}
-	         });
-	    });
+    $(function(){        
+        $("#extruderTop").buildMbExtruder({
+                    position:"top",
+                    width:350,
+                    extruderOpacity:1,
+                    onExtOpen:function(){},
+                    onExtContentLoad:function(){},
+                    onExtClose:function(){}
+             });
+        });
     
     //updateOrientation();
 });
 
 function helpScreen()  {
-	console.log("Display Help.");
-	$("#help-dialog").dialog('open') 
+    console.log("Display Help.");
+    $("#help-dialog").dialog('open') 
 
 }
 
@@ -132,40 +132,40 @@ function switchLesson(incr)
 
 function checkForNewData()
 {
-	$.post("getResults.php", 'offset='+ trym2.offset, function(data){
+    $.post("getResults.php", 'offset='+ trym2.offset, function(data){
 
-		if(data != "")
-		{
-			$("#M2Out").val($("#M2Out").val() + data); 
+        if(data != "")
+        {
+            $("#M2Out").val($("#M2Out").val() + data); 
             scrollDown( "#M2Out" );
-			trym2.offset = trym2.offset + data.length;
-			trym2.waitingtime = trym2.minwaitingtime;
-		} 
-		else
-		{
-		    trym2.waitingtime = 2*trym2.waitingtime;
-		    if (trym2.waitingtime > trym2.maxwaitingtime)
-		    {
-		        trym2.waitingtime = trym2.maxwaitingtime;
-		    }
-		}
+            trym2.offset = trym2.offset + data.length;
+            trym2.waitingtime = trym2.minwaitingtime;
+        } 
+        else
+        {
+            trym2.waitingtime = 2*trym2.waitingtime;
+            if (trym2.waitingtime > trym2.maxwaitingtime)
+            {
+                trym2.waitingtime = trym2.maxwaitingtime;
+            }
+        }
         $("#waittime").text("waiting time: " + trym2.waitingtime);
-    	trym2.timerobject = setTimeout("checkForNewData()",trym2.waitingtime);
-	});
+        trym2.timerobject = setTimeout("checkForNewData()",trym2.waitingtime);
+    });
 
 }
 
 
 
 function sendOnEnterCallback( inputfield ) {
-	return function(e) {
-		if (e.which == 13 && e.shiftKey) {
+    return function(e) {
+        if (e.which == 13 && e.shiftKey) {
             e.preventDefault();
             // do not make a line break or remove selected text when sending
 
             sendToM2(">>SENDCOMMANDS<<\n"+getSelected( inputfield ), "You hit shift-enter!! ");
         }
-	}
+    }
 }
 
 function resetCallback(e) {
@@ -176,11 +176,11 @@ function resetCallback(e) {
 }
 
 function sendCallback( inputField ) {
-	return function(e) {
-    	var str = getSelected( inputField );
-	    sendToM2(">>SENDCOMMANDS<<\n"+str, "");
-	    return false;
-	}
+    return function(e) {
+        var str = getSelected( inputField );
+        sendToM2(">>SENDCOMMANDS<<\n"+str, "");
+        return false;
+    }
 }
 
 // return false on error
@@ -194,7 +194,7 @@ function sendToM2(myCommand, baseString) {
         cmd: myCommand
     },
     function(data) {
-		console.log("Here is the data: " + data);
+        console.log("Here is the data: " + data);
         if (data != "0") { 
             $("#M2Out").val($("#M2Out").val() + "Something Broke! HELP!");
             return false;
@@ -230,21 +230,21 @@ function getSelected( inputField) {
 
 // input: filename with tutorial content
 // return a list of lessons title, each wrapped in a div and link, 
-// <div><a>" + title + "</a></div>	
+// <div><a>" + title + "</a></div>  
 // attach a lesson ID
-function getLessonTitles( tutorialFile, callback ){	
-	var titles = "";
-	$("#menuTutorial").load(tutorialFile, function(){
-		var i = 1;
-		$("#menuTutorial h4").each( function() {
-			var title = $(this).text();
-			titles = titles + "<div><a class='submenuItem' lessonid='lesson" + i +"'>" + title + "</a></div>";	
-			i = i + 1;
-			//console.log("Title in m2.js: " + title);
-		});
-		//console.log("All titles: " + titles);
-		callback(titles);
-	});
+function getLessonTitles( tutorialFile, callback ){ 
+    var titles = "";
+    $("#menuTutorial").load(tutorialFile, function(){
+        var i = 1;
+        $("#menuTutorial h4").each( function() {
+            var title = $(this).text();
+            titles = titles + "<div><a class='submenuItem' lessonid='lesson" + i +"'>" + title + "</a></div>";  
+            i = i + 1;
+            //console.log("Title in m2.js: " + title);
+        });
+        //console.log("All titles: " + titles);
+        callback(titles);
+    });
 }
 
 function updateOrientation()
@@ -263,8 +263,8 @@ function updateOrientation()
             orient = "show_landscape";
     }
     $("body").attr("class", orient);
-	$("#rightwindow").attr('class', orient);
-	$("#leftwindow").attr('class', orient);
+    $("#rightwindow").attr('class', orient);
+    $("#leftwindow").attr('class', orient);
 }
 /* Info on selected text:
  * jquery plugin: jquery-fieldselection.js

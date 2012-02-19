71c0250e5b2eafcf8990c16ba53194e256faffe0
Franziska Hinkelmann <fhinkel@vt.edu>
2012-02-18 20:33:48 -0500

    convert m2 doc preamble to html head

--- converter.rb
+++ converter.rb
@@ -3,10 +3,23 @@ require 'pp'
 isCode = false
 isExample = false
 isInLesson = false
+isInPreamble = true
+isHeadline = false
 
 open("Beginning.html","w") { |out|
+  out << "<html>\n"
+  out << "  <head>\n"
+  out << '    <linkrel="stylesheet"href="m2.css"type="text/css"media="screen">'
+  out << "\n"
+  out << "    <title>\n"
   File.open("BeginningCopy.html").each { |line|
       case line 
+      when /Headline/
+        isHeadline = true
+        next
+      when /Description/
+        isInPreamble = false
+        next
       when /^\s*Example\s*$/
         isExample = true
         out << "<br>"
@@ -20,27 +33,38 @@ open("Beginning.html","w") { |out|
         #out << "<BR>\n"
         next
       else
-        if isExample 
-          out << "<code>" << line.strip << "</code></br>\n"
-        elsif isCode
-          case line
-          when /SUBSECTION/
-            if isInLesson
-              out << "</div>"
+        if isHeadline
+          out << line
+          out << "    </title>\n"
+          out << "  </head>\n"
+          out << "<body>\n"
+          isHeadline = false
+        end
+        unless isInPreamble 
+          if isExample 
+            #puts line.lstrip.rstrip
+            out << "<code>" << line.strip << "</code></br>\n"
+          elsif isCode
+            case line
+            when /SUBSECTION/
+              if isInLesson
+                out << "</div>"
+              end
+              out << "<div>"
+              isInLesson = true
+              puts line.match(/\s*SUBSECTION\s*"(.*)"/)[1]
+              out << "<h4>" << line.match(/\s*SUBSECTION\s*"(.*)"/)[1] << "</h4>\n"
+            else
+              out << line
             end
-            out << "<div>"
-            isInLesson = true
-            puts line.match(/\s*SUBSECTION\s*"(.*)"/)[1]
-            out << "<h4>" << line.match(/\s*SUBSECTION\s*"(.*)"/)[1] << "</h4>\n"
           else
             out << line
           end
-            
-        else
-          out << line
         end
-      
       end
   }
   out << "</div>"
+  out << "</body>"
+  out << "</html>"
+  
 }

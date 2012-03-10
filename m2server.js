14496076e5c7549a2539349c13432e694f368e38
Mike Stillman <mikestillman1@gmail.com>
2012-03-09 19:30:53 -0500

    works with cookies and several clients.  Mostly

--- m2server.js
+++ m2server.js
@@ -6,52 +6,49 @@
 var http = require('http');  // NodeJS HTTP server API
 var Cookies = require("cookies");
 
-
 // The HTML file for the chat client. Used below.
 var clientui = require('fs').readFileSync("index.html");
-// var emulation = require('fs').readFileSync("EventSourceEmulation.js");
 
 // An array of ServerResponse objects that we're going to send events to
 var clients = [];
+var nextClientId = 0;
 
-var clientIds = [];
-var clientId = 0;
+function Client(m2process, resp) {
+    this.m2 = m2process;
+    this.response = resp;
+}
+
+startM2Emitter = function(client) {
+    client.m2 = startM2();
+    console.log("starting emitter");
+    client.m2.stdout.on('data', function (data) {
+        //console.log('m2stdout: ' + data);
+        message = 'data: ' + data.replace(/\n/g, '\ndata: ') + "\r\n\r\n";
+        client.response.write(message);           
+    });
+    client.m2.stderr.on('data', function (data) {
+        //console.log('m2stderr: ' + data);
+        message = 'data: ' + data.replace(/\n/g, '\ndata: ') + "\r\n\r\n";
+        client.response.write(message); 
+    });    
+};
 
 // Send a comment to the clients every 20 seconds so they don't 
 // close the connection and then reconnect
 setInterval(function() {
     clients.forEach(function(client) {
-        client.write(":ping\n");
+        client.response.write(":ping\n");
     });
 }, 20000);
 
-var m2;
-startM2Emitter = function(client) {
-    var myM2 = client.myM2;
-    myM2.stdout.on('data', function (data) {
-           //console.log('m2stdout: ' + data);
-           message = 'data: ' + data.replace(/\n/g, '\ndata: ') + "\r\n\r\n";
-           //console.log('m2stdout message: ' + message);
-           client.write(message); 
-           console.log("This is the Id we attached to a response object: " + client.myId);
-          
-       });
-       myM2.stderr.on('data', function (data) {
-           console.log('m2stdout: ' + data);
-           message = 'data: ' + data.replace(/\n/g, '\ndata: ') + "\r\n\r\n";
-           //console.log('m2stdout message: ' + message);
-           client.write(message); 
-           console.log("This is the Id we attached to a response object: " + client.myId);
-       });    
-};
 
 startM2 = function() {
     var spawn = require('child_process').spawn;
-    m2 = spawn('M2');
+    var m2 = spawn('M2');
     m2.stdout.setEncoding("utf8");
     m2.stderr.setEncoding("utf8");
     console.log('Spawned m2 pid: ' + m2.pid);
-   
+    return m2;
 };
 
 
@@ -66,15 +63,10 @@ server.on("request", function (request, response) {
    
     // If the request was for "/", send the client-side chat UI.
     if (url.pathname === "/" || url.pathname === "/index.html") {  // A request for the chat UI
-        cookies.set( "trym2cookie", clientId.toString(10), { httpOnly: false } );
-        console.log("Here is a new client with these headers: ");
-        for (e in request.headers) {
-                console.log(e+": "+request.headers[e]);
-        }
-        clientIds.push(clientId);
-        clientId = clientId + 1;
-        startM2();
-        
+        cookies.set( "trym2cookie", nextClientId.toString(10), { httpOnly: false } );
+        clients.push(new Client); // will be populated with a Client in /chat
+        nextClientId = nextClientId + 1;
+          
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write(clientui);
         response.end();
@@ -83,53 +75,50 @@ server.on("request", function (request, response) {
     if (/\.css/.test(url.pathname) ) {
         response.writeHead(200, {'Content-Type': 'text/css'});
         var u = url.pathname.replace("/", '');
-        //console.log("Filenmae: " + u);
         response.write(require('fs').readFileSync(u));
         response.end();
         return;
-        //response.writeHead(404);
-        //response.end();
-        //return;
     }
     if (url.pathname === "/chat") {
         // If the request was a post, then a client is posting a new message
+        var clientID = parseInt(cookies.get("trym2cookie"),10);
+        console.log("cookie value from client: " + clientID);
         if (request.method === "POST") {
             request.setEncoding("utf8");
             
-            for (e in request.headers) {
-                console.log(e+": "+request.headers[e]);
-            }
+            //   for (e in request.headers) {
+            //       console.log(e+": "+request.headers[e]);
+            //   }
 
             var body = "";
             // When we get a chunk of data, add it to the body
             request.on("data", function(chunk) { body += chunk; });
 
-            // When the request is done, send an empty response 
-            // and broadcast the message to all listening clients.
-            request.on("end", function() {
+           request.on("end", function() {
                 response.writeHead(200);   // Respond to the request
                 response.end();
                 // Send 'body' to M2
-                m2.stdin.write(body);
+                clients[clientID].m2.stdin.write(body);
             });
             return;
         } else {
             // Set the content type and send an initial message event 
             response.writeHead(200, {'Content-Type': "text/event-stream" });
-            response.myId = "hello " + clientId;
-            response.myM2 = m2;
             //response.write("data: Connected, starting M2 ...\ndata: \n\n");
-            startM2Emitter(response);
+            console.log("in /chat: " + clients[clientID].response);
+            if (!clients[clientID].response) {
+                clients[clientID].response = response;
+                console.log("in /chat: " + clients[clientID].response);
+                startM2Emitter(clients[clientID]);
+            }
 
             // If the client closes the connection, remove the corresponding
             // response object from the array of active clients
             request.connection.on("end", function() {
-                clients.splice(clients.indexOf(response), 1);
+                //TODO: kill client gracefully
                 response.end();
             });
 
-            // Remember the response object so we can send future messages to it
-            clients.push(response);
             return;
         }
     }
@@ -137,10 +126,8 @@ server.on("request", function (request, response) {
     if (url.pathname !== "/chat") {
         
         var u = url.pathname.replace("/", '');
-        //console.log("Filenmae: " + u);
         try {
             data = require('fs').readFileSync(u);
-            //console.log(data);
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write(data);
         }
@@ -158,9 +145,6 @@ server.on("request", function (request, response) {
   
 });
 
-
-
 // Run the server on port 8000. Connect to http://localhost:8000/ to use it.
-//server.listen(8000);
 console.log("Listening on port 8000...");
 server.listen(8000);

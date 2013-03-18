4cba24fea34739f606fe2a79940167f68eb27863
Lars Kastner <lars.kastner@yahoo.dk>
2013-03-19 00:53:51 +0100

    Zombie stuff.

--- tests/index_zombie.js
+++ tests/index_zombie.js
@@ -1,24 +1,26 @@
 
 var Browser = require("zombie");
-var assert = require("assert");
+//var assert = require("assert");
 
 // Load the page from localhost
-browser = new Browser()
-browser.visit("http://habanero.math.cornell.edu:3690/",{runScripts: true, waitFor: 10000, maxWait: 30000}, function () {
+browser = new Browser();
+browser.visit("http://tryruby.org/levels/1/challenges/0",{runScripts: true, waitFor: 5000, maxWait: 10000}, function () {
    
 //   browser.pressButton(browser.button("reset"));
-   var item = browser.document.getElementById("M2In");
-   console.log(item);
-   console.log(item.attributes);   
-   console.log(item.value);   
+   //var item = browser.document.getElementById("tutorial");
+   //console.log(item.html);
+   //console.log(item.attributes);   
+   //console.log(item.value);
+   //console.log(browser.body);
       // Form submitted, new page loaded.
-   console.log(browser.text("title"));
-   console.log(browser.text("html"));
-   console.log(browser.text("M2Out"));
+   //console.log(browser.text("title"));
+   console.log(browser.html("html"));
+   //console.log(browser.html("#selectTutorialLink"));
+   //console.log(browser.text("M2Out"));
 
-})
-browser.wait(20000, function() {
-   var item = browser.document.getElementById("M2Out");
+});/*
+browser.wait(5000, function() {
+   var item = browser.document.getElementById("M2In");
    console.log(item);
    console.log(item.attributes);
    console.log(item.value);
@@ -28,4 +30,4 @@ browser.wait(20000, function() {
    console.log(browser.text("M2Out"));
 
 
-});
+});*/

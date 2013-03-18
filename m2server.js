88b5b0deaf2adb08fbb438aaa0a60d9035259bb1
Lars Kastner <lars.kastner@yahoo.dk>
2013-03-18 23:30:22 +0100

    Messing with cgroups.

--- m2server.js
+++ m2server.js
@@ -166,7 +166,7 @@ m2Start = function(clientID) {
     var spawn = require('child_process').spawn;
     if (SCHROOT) {
     	var m2 = spawn( 'sudo',
-      [ '-u', clientID, 'schroot', '-c', clientID, '-u', clientID, '-d', '/home/m2user/', '-r', '/bin/bash', '/M2/limitedM2.sh']);
+      [ 'cgexec', '-g', 'memory:'+clientID, 'sudo', '-u', clientID, 'schroot', '-c', clientID, '-u', clientID, '-d', '/home/m2user/', '-r', '/bin/bash', '/M2/limitedM2.sh']);
         
            // ['-c', clientID, '-u', 'm2user', '-d', '/home/m2user/', '-r', '/bin/bash', '/M2/limitedM2.sh']);
     } else {

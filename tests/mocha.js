c539d9be2bba86bee946033cc48774b012fe8f4f
Franziska Hinkelmann <hinkelmann.1@mbi.osu.edu>
2013-03-19 14:21:18 -0400

    test for dummy server in mocha

--- tests/mocha.js
+++ tests/mocha.js
@@ -1,5 +1,6 @@
-var assert = require("assert")
-var m2server = require('../m2server')
+var assert = require("assert");
+var http = require('http');
+var m2server = require('../m2server');
 
 describe('Array', function(){
   describe('#indexOf()', function(){
@@ -7,8 +8,8 @@ describe('Array', function(){
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
-    it('should not be true', function() {
-        assert(false);
+    it('should be true', function() {
+        assert(true);
     }); 
   })
 })
@@ -20,4 +21,44 @@ describe('m2server', function(){
         assert(true);
     })
 })
+
+describe('testserver', function() {
+    var server;
+    before(function(done) {
+        var http = require('http');
+        server = http.createServer(function (req, res) {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.end('Hello World\n');
+        }).listen(1337, '127.0.0.1');
+        console.log('Server running at http://127.0.0.1:1337/');
+        
+        server.on("listening", function() {
+            console.log("started");
+            done();
+        });
+    });
+    it("Should fetch /", function(done) {
+        http.get("http://localhost:1337/", function(res) {
+            assert.equal(res.statusCode, 200);
+            done();
+        });
+    });
+    it("Should fetch Hello World", function(done) {
+        http.get("http://localhost:1337/", function(res) {
+            res.on('data', function(body) {
+                console.log(body);
+                assert.equal(body, "Hello World\n");
+                done();
+            });
+        });
+    });
+        
+    after(function(done){
+      server.close();
+      console.log("stopped");
+      done();
+    });
+});
+
+
 

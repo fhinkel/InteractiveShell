f2fb032ba7d569ef6a0f02e901a634b9bf96d216
Lars Kastner <lars.kastner@yahoo.dk>
2013-03-20 03:31:30 +0100

    I do not understand what this is doing at all, but maybe it does the right thing.

---
+++ tests/test-jsdom.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+var jsdom = require('jsdom');
+var doc   = jsdom.jsdom(fs.readFileSync("test.html"), null, {
+          features: {
+            FetchExternalResources   : ['script'],
+            ProcessExternalResources : ['script'],
+            MutationEvents           : '2.0',
+        }
+    });
+
+var window = doc.createWindow();
+jsdom.jQueryify(window, "http://code.jquery.com/jquery-1.5.min.js", function() {
+    console.log(window.document.body.innerHTML);
+    //console.log(window.$().jquery); //jquery version
+});
+
+/*
+var jsdom = require("jsdom");
+var window = jsdom.jsdom().createWindow();
+
+jsdom.jQueryify(window, "http://habanero.math.cornell.edu:3690/", function () {
+  window.$("body").append('<div class="testing">Hello World, It works</div>');
+
+  console.log(window.document.body.innerHTML);
+});
+
+
+var jsdom = require("jsdom").jsdom;
+
+
+
+jsdom.env("http://habanero.math.cornell.edu:3690/", ["http://code.jquery.com/jquery.min.js"], function(err, window) {
+    // jQuery is at window.$
+//   console.log(window);
+   console.log(window.document.title);
+   console.log(window.document.body.onload);
+   console.log(window.document.body.innerHTML);
+   //console.log("contents of M2Out:", window.$("M2Out"));
+});*/
+

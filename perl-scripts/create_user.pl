d4a2f74e49c2f3b20fca541f979a094a689cb37b
Lars Kastner <lars.kastner@yahoo.dk>
2014-01-07 19:36:27 +0100

    Modifying create user script to get rid of schroot source mods.

--- perl-scripts/create_user.pl
+++ perl-scripts/create_user.pl
@@ -47,9 +47,15 @@ print CONFIG "directory=/fakeroot/clone\n";
 print CONFIG "root-users=\n";
 print CONFIG "type=directory\n";
 print CONFIG "users=$user\n";
-print CONFIG "profile=clone\n";
+print CONFIG "profile=$schroot_type\n";
 close (CONFIG); 
 
+
+################################
+##  Schroot fstab stuff
+################################
+system "cp -r /usr/local/etc/schroot/clone /usr/local/etc/schroot/$schroot_type";
+system "sed -i \"s/\\\$FULL_MOUNT_PATH/\/usr\/local\/var\/lib\/schroot\/mount\/$schroot_type/g\" /usr/local/etc/schroot/$schroot_type/fstab";
 
 
 ################################

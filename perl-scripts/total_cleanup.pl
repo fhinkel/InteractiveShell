dd11adb3045a930cad8239736c6d2a3eaa1a34e4
gentoo_admin <bla@blubb.de>
2014-01-07 09:35:33 -0500

    Fixed duplicate session problem.

--- perl-scripts/total_cleanup.pl
+++ perl-scripts/total_cleanup.pl
@@ -5,6 +5,7 @@ $n = @users;
 print "\nRemoving ",$n," users.\n";
 system("schroot -e -f --all-sessions");
 system("rm /usr/local/etc/schroot/chroot.d/system*");
+system("rm -rf /usr/local/etc/schroot/system*");
 system("rm /usr/local/etc/schroot/chroot.d/user*");
 $users = @users[@users-1];
 @users = split(",",$users);

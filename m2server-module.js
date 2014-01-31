e2379d14fc1e5de59ff152e1491db68e70139d5f
Franziska Hinkelmann <hinkelmann.1@mbi.osu.edu>
2014-01-31 09:40:48 +0100

    different background color

--- m2server-module.js
+++ m2server-module.js
@@ -226,7 +226,6 @@ var M2Server = function (overrideOptions) {
         if (options.SCHROOT) {
             m2 = spawnSchroot(clientID, 'Singular');
         } else {
-
             m2 = spawn('script', ['/dev/null', 'Singular', '-t']);
             //m2 = spawn('Singular', ['-t']);
         }

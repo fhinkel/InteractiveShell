d5075b926f767c7d6433b17c75a4990ba5c5865f
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-04-08 19:52:25 -0400

    Fix test

--- tests/docker.js
+++ tests/docker.js
@@ -5,16 +5,13 @@ var spawn = require('child_process').spawn;
 describe('Start docker container', function () {
     it('should show M2 preamble', function (done) {
         var docker = "docker";
-        var process = spawn(docker, ["run", "fhinkel/macaulay2", "M2 -e 'exit 0;'"]);
+        var process = spawn(docker, ["run", "fhinkel/macaulay2", "M2", "-e", "exit 0;"]);
         var encoding = "utf8";
         process.stderr.setEncoding(encoding);
         var result = "";
         process.stderr.on('data', function (data) {
             console.log("Preamble: " + data);
             result += data;
-            assert(result.match(/Macaulay2, version 1\.\d/),
-                'M2 preamble does not match');
-            done();
         });
         process.on('error', function (error) {
             assert(false, error);

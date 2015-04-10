4d76eb37073bca6894c874761ce40029cdcf73c6
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-04-10 17:32:05 -0400

    Add tests for getTutorialList with stubbed file system

--- tests/mathServer.js
+++ tests/mathServer.js
@@ -3,7 +3,7 @@ var http = require('http');
 var mathServer = require('../lib/mathServer.js');
 var sinon = require('sinon');
 
-describe.only('MathServer Module test', function () {
+describe('MathServer Module test', function () {
     describe('When we call getTutorialList', function (next) {
         it('should get the list', function (done) {
             var server = mathServer.MathServer();
@@ -15,7 +15,53 @@ describe.only('MathServer Module test', function () {
                 }
             };
             var spy = sinon.spy(response, "end");
-            server.getListOfTutorials(null, response);
+            var fs = require('fs');
+            server.getListOfTutorials(fs)(null, response);
+        });
+        it('should get the list from mocked file system with shared tutorials', function (done) {
+            var server = mathServer.MathServer();
+            var response = {
+                writeHead: function() {},
+                end: function() {
+                    var expected = JSON.stringify(["tutorials/mock.html",
+                    "shared-tutorials/mock.html"
+                    ]);
+                    assert(spy.withArgs(expected).calledOnce);
+                    readdirStub.restore();
+                    existsStub.restore();
+                    done();
+                }
+            };
+            var fs = require('fs');
+            var readdirStub = sinon.stub(fs, 'readdir');
+            readdirStub.yields(null, ['mock.html', 'nothtml.foo']);
+            var existsStub= sinon.stub(fs, 'exists');
+            existsStub.yields(true);
+
+            var spy = sinon.spy(response, "end");
+            server.getListOfTutorials(fs)(null, response);
+
+        });
+        it('should get the list from mocked file system without shared tutorials', function (done) {
+            var server = mathServer.MathServer();
+            var response = {
+                writeHead: function() {},
+                end: function() {
+                    var expected = JSON.stringify(["tutorials/mock.html"]);
+                    assert(spy.withArgs(expected).calledOnce);
+                    readdirStub.restore();
+                    existsStub.restore();
+                    done();
+                }
+            };
+            var fs = require('fs');
+            var readdirStub = sinon.stub(fs, 'readdir');
+            readdirStub.yields(null, ['mock.html', 'nothtml.foo']);
+            var existsStub= sinon.stub(fs, 'exists');
+            existsStub.yields(false);
+
+            var spy = sinon.spy(response, "end");
+            server.getListOfTutorials(fs)(null, response);
         });
     });
 
@@ -38,7 +84,6 @@ describe.only('MathServer Module test', function () {
             var sorted = server.sortTutorials(tutorials, 'x');
             assert.equal(JSON.stringify(sorted), JSON.stringify(['c', 'b', 'a']));
         });
-
     });
 
     describe('Stringify', function() {

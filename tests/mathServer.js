a1886a011fe86c4a6c01f555e2fffe848e1c8328
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-04-10 16:13:10 -0400

    More tests

--- tests/mathServer.js
+++ tests/mathServer.js
@@ -3,14 +3,10 @@ var http = require('http');
 var mathServer = require('../lib/mathServer.js');
 var sinon = require('sinon');
 
-describe('MathServer Module test', function () {
+describe.only('MathServer Module test', function () {
     describe('When we call getTutorialList', function (next) {
         it('should get the list', function (done) {
-            var server = mathServer.MathServer({
-                port: 8006,
-                CONTAINERS: './sudo_docker_containers.js'
-            });
-
+            var server = mathServer.MathServer();
             var response = {
                 writeHead: function() {},
                 end: function() {
@@ -18,11 +14,31 @@ describe('MathServer Module test', function () {
                     done();
                 }
             };
-
             var spy = sinon.spy(response, "end");
-
             server.getListOfTutorials(null, response);
         });
+    });
+
+    describe('When calling moveWelcomeTutorialToBeginning', function() {
+        it('shoud move the tutorial to the beginning', function() {
+            var server = mathServer.MathServer();
+            var tutorials = ['a', 'b', 'c'];
+            var sorted = server.sortTutorials(tutorials, 'b');
+            assert.equal(JSON.stringify(sorted), JSON.stringify(['b', 'a', 'c']));
+        });
+        it('shoud move the tutorial to the beginning and keep the others', function() {
+            var server = mathServer.MathServer();
+            var tutorials = ['c', 'b', 'a'];
+            var sorted = server.sortTutorials(tutorials, 'b');
+            assert.equal(JSON.stringify(sorted), JSON.stringify(['b', 'c', 'a']));
+        });
+        it('shoud move do nothing it index not found', function() {
+            var server = mathServer.MathServer();
+            var tutorials = ['c', 'b', 'a'];
+            var sorted = server.sortTutorials(tutorials, 'x');
+            assert.equal(JSON.stringify(sorted), JSON.stringify(['c', 'b', 'a']));
+        });
+
     });
 
     describe('Stringify', function() {

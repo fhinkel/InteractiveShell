104ae2fbafc15c627177557d39ed407f45759a7e
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-04-09 21:20:07 -0400

    Using exit instead of close

--- tests/docker.js
+++ tests/docker.js
@@ -21,9 +21,6 @@ describe('Start docker container', function () {
         });
         process.on('exit', function (error) {
             console.log('EXIT for -t version');
-        });
-        process.on('close', function() {
-            console.log('on close in version' + result);
             if (result.match(/1\.\d/)) {
                 assert(true);
                 next()
@@ -39,28 +36,23 @@ describe('Start docker container', function () {
                 next();
             }
         });
+        process.on('close', function() {
+            console.log('on close in version' + result);
+        });
     });
     it('should show M2 version without -t', function (next) {
         var process = spawn("docker", ["run", "fhinkel/macaulay2", "M2", "--version"]);
         process.stdout.setEncoding("utf8");
-        process.stderr.setEncoding("utf8");
         var result= '';
         process.stdout.on('data', function (data) {
             result += data;
             console.log('Version: ' + result);
         });
-        process.stderr.on('data', function (data) {
-            result += data;
-            console.log('Version: ' + result);
-        });
-        process.on('exit', function (error) {
-            console.log('EXIT for -t version');
-        });
         process.on('error', function (error) {
             console.log('on error');
         });
-        process.on('close', function() {
-            console.log('on close in version' + result);
+        process.on('exit', function() {
+            console.log('on exit in version without -t' + result);
             if (result.match(/1\.\d/)) {
                 assert(true);
                 next()
@@ -88,14 +80,11 @@ describe('Start docker container', function () {
         process.on('disconnected', function (error) {
             console.log('DISCONNECTED for preamble');
         });
-        process.on('exit', function (error) {
-            console.log('EXIT for preamble');
-        });
         process.on('error', function (error) {
             console.log('on error');
         });
-        process.on('close', function() {
-            console.log('on close in echo');
+        process.on('exit', function() {
+            console.log('on exit in echo');
             if (result.match(/hello/)) {
                 assert(true);
                 next()
@@ -123,8 +112,8 @@ describe('Start docker container', function () {
         process.on('error', function (error) {
             console.log('on error');
         });
-        process.on('close', function() {
-            console.log('Close in preamble');
+        process.on('exit', function() {
+            console.log('In exit in preamble');
             if (result.match(/Macaulay2, version 1\.\d/)) {
                 assert(true);
                 next()
@@ -138,6 +127,19 @@ describe('Start docker container', function () {
             else {
                 assert(false, 'M2 preamble did not match: ' + result);
                 next();
+            }
+        });
+        process.on('close', function() {
+            console.log('Close in preamble');
+            if (result.match(/Macaulay2, version 1\.\d/)) {
+                assert(true);
+            } else if (result.match(/no such file or directory. Are you trying to connect to a TLS-enabled daemon without TLS/)) {
+                assert(false, 'Error starting Docker container, is docker installed?\n' + result);
+            } else if (result.match(/\/run\/docker.sock: permission denied/)) {
+                assert(false, 'Error starting Docker container, permission denied. Maybe run test as root?\n' + result);
+            }
+            else {
+                assert(false, 'M2 preamble did not match: ' + result);
             }
         });
     });

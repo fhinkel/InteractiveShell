d46f1de53677fa00fbee46251ed0ece31bb9a118
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-04-10 16:00:35 -0400

    Improve test for welcome tutorial

--- tests/mathServer.js
+++ tests/mathServer.js
@@ -14,14 +14,17 @@ describe('MathServer Module test', function () {
             var response = {
                 writeHead: function() {},
                 end: function() {
+                    assert(spy.withArgs(sinon.match(/tutorials\/welcome2.html/)).calledOnce);
                     done();
                 }
             };
 
-            server.getListOfTutorials(null, response);
+            var spy = sinon.spy(response, "end");
 
+            server.getListOfTutorials(null, response);
         });
     });
+
     describe('Stringify', function() {
         it('should stringify an array', function() {
             var tutorials = ["a", "b"];

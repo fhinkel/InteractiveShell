5f35ea8ae2625eb52be4d755badb144150de5335
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-04-09 20:38:23 -0400

    I do not even know anymore

--- tests/docker.js
+++ tests/docker.js
@@ -66,7 +66,7 @@ describe('Start docker container', function () {
     });
 
     it('should show M2 preamble', function (next) {
-        var process = spawn("docker", ["run", "fhinkel/macaulay2", "M2", "-e",  "exit\ 0;", "&", "docker", "rm", "$(docker\ ps\ -q)"]);
+        var process = spawn("docker", ["run", "fhinkel/macaulay2", "M2", "-e",  "exit\ 0;", "&", "/bin/echo", "hello", "&", "docker", "rm", "$(docker\ ps\ -q)"]);
         process.stderr.setEncoding("utf8");
         process.stdout.setEncoding("utf8");
         var result = '';

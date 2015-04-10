a9246ba33341928e907ad3f1519aeb9be6dc3d9a
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-04-09 21:03:16 -0400

    I do not even know anymore

--- tests/docker.js
+++ tests/docker.js
@@ -37,6 +37,40 @@ describe('Start docker container', function () {
             }
         });
     });
+    it('should show M2 version without -t', function (next) {
+        var process = spawn("docker", ["run", "fhinkel/macaulay2", "M2", "--version"]);
+        process.stdout.setEncoding("utf8");
+        process.stderr.setEncoding("utf8");
+        var result= '';
+        process.stdout.on('data', function (data) {
+            result += data;
+            console.log('Version: ' + result);
+        });
+        process.stderr.on('data', function (data) {
+            result += data;
+            console.log('Version: ' + result);
+        });
+        process.on('error', function (error) {
+            console.log('on error');
+        });
+        process.on('close', function() {
+            console.log('on close in version' + result);
+            if (result.match(/1\.\d/)) {
+                assert(true);
+                next()
+            } else if (result.match(/no such file or directory. Are you trying to connect to a TLS-enabled daemon without TLS/)) {
+                assert(false, 'Error starting Docker container, is docker installed?\n' + result);
+                next();
+            } else if (result.match(/\/run\/docker.sock: permission denied/)) {
+                assert(false, 'Error starting Docker container, permission denied. Maybe run test as root?\n' + result);
+                next();
+            }
+            else {
+                assert(false, 'M2 version: ' + result);
+                next();
+            }
+        });
+    });
 
     it('should return after echo', function (next) {
         var process = spawn("docker", ["run", "fhinkel/macaulay2", "/bin/echo", "hello"]);

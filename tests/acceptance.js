fd426656d2f56fae7dea08e81d4f2b2a792dbcf9
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-04-11 10:14:50 -0400

    Use supertest module for acceptance test

--- tests/acceptance.js
+++ tests/acceptance.js
@@ -1,26 +1,44 @@
-var assert = require("assert");
+var assert = require('chai').assert;
 var http = require('http');
 var mathServer = require('../lib/mathServer.js');
+var request = require('supertest');
 
-describe('Acceptance test', function (next) {
-    describe('As basic behavior we', function (next) {
+describe('Acceptance test', function () {
+    var port = 8006;
+    var server;
+
+    before(function () {
+        server = mathServer.MathServer({
+            port: port,
+            CONTAINERS: './dummy_containers.js'
+        });
+        server.listen();
+    });
+
+    after(function (done) {
+        server.close();
+        done();
+    });
+
+    describe('As basic behavior we', function () {
         it('should be able to create server and get title from html body', function (done) {
-            var server = mathServer.MathServer({
-                port: 8006,
-                CONTAINERS: './dummy_containers.js'
-            });
-            server.listen();
-            http.get("http://127.0.0.1:8006", function (res) {
+            http.get("http://127.0.0.1:" + port, function (res) {
                 res.on('data', function (body) {
                     var str = body.toString('utf-8');
                     var n = str.match(/<title>\s*([^\s]*)\s*<\/title>/);
                     assert.equal(n[1], 'Macaulay2');
-                });
-                res.on('end', function() {
-                    server.close();
                     done();
                 });
             });
+        });
+        it('should show title', function (done) {
+            request = request('http://localhost:' + port);
+
+            request.get('/').expect(200).end(function(error, result) {
+                assert.match(result.text, /<title>\s*Macaulay2\s*<\/title>/);
+                done();
+            });
+
         });
     });
 });

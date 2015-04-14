1b89b2f64b6c15945558f3e2fb5b5863af3489da
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-04-13 22:45:20 -0400

    Review of sudoDockerContainers including basic tests

--- src/lib/sudoDockerContainers.js
+++ src/lib/sudoDockerContainers.js
@@ -1,7 +1,7 @@
-var sudo_docker_manager = function () {
-    //var exec = require('child_process').exec;
-    
-   
+var exec = require('child_process').exec;
+var waitForSshd;
+
+var dockerManager = function () {
     var resources = {
         cpus: 1,
         memory: 128
@@ -18,77 +18,75 @@ var sudo_docker_manager = function () {
         }
     };
 
-
-
-
-    var removeInstance = function(instance){
-      console.log("Removing container: " + instance.containerName);
-      var exec = require('child_process').exec;
-       exec('sudo docker rm -f ' + instance.containerName, function(error, stdout, stderr){
-         console.log("Removed container with result: " + error);
-       });
-       delete instance;
-    };
-   
-    var getNewInstance = function (next) {
-      var exec = require('child_process').exec;
-      var currentInstance = JSON.parse(JSON.stringify(options.instance));
-      options.instance.port++;
-      var dockerContainerName = "m2Port" + currentInstance.port;
-      var dockerRunCmd = 'sudo docker run -d';
-      dockerRunCmd += ' -c ' + resources.cpus;
-      dockerRunCmd += ' -m ' + resources.memory + 'm';
-      dockerRunCmd += ' --name ' + dockerContainerName;
-      dockerRunCmd += ' -p ' + currentInstance.port + ':22';
-      dockerRunCmd += ' m2container ' + options.sshdCmd;
-      // console.log("Ports: " + currentInstance.port + " " + options.instance.port);
-      exec(dockerRunCmd, function (error, stdout, stderr) {
-         if(error){
-            console.log("There was an error starting the docker container: " + error.message);
-            if(error.message.match(/Conflict. The name/)){
-               console.log("The name was taken.");
-               getNewInstance(next);
-            } else {
-               throw error;
+    var removeInstance = function (instance) {
+        console.log("Removing container: " + instance.containerName);
+        var removeDockerContainer = 'sudo docker rm -f ' + instance.containerName;
+        exec(removeDockerContainer, function (error, stdout, stderr) {
+            if (error) {
+                console.error("Error removing container " + instance.containerName + ' with error:' + error)
             }
-         } else {
-            //console.log("Starting ssh. OUT: " + stdout + " ERR: " + stderr);
-            currentInstance.containerName = dockerContainerName;
-            waitForSshd(next, currentInstance, dockerContainerName);
-         }
-      });
+        });
     };
-   
-    var waitForSshd = function(next, instance, dockerContainerName){
-      var exec = require('child_process').exec;
-      var dockerRunningProcesses = "sudo docker exec " + dockerContainerName + " ps aux";
-      var filterForSshd = "grep \"" + options.sshdCmd + "\"";
-      var excludeGrep = "grep -v grep";
-      
-      exec(dockerRunningProcesses + " | " + filterForSshd + " | " + excludeGrep, function(error, stdout, stderr){
-         if(error){
-            console.log("There was an error while waiting for sshd: " + error);
-         }
-         var RunningSshDaemons = stdout;
 
-         console.log("Looking for sshd. OUT: " + stdout + " ERR: " + stderr);
+    var constructDockerRunCommand = function(resources, currentInstance) {
+        var dockerRunCmd = 'sudo docker run -d';
+        dockerRunCmd += ' -c ' + resources.cpus;
+        dockerRunCmd += ' -m ' + resources.memory + 'm';
+        dockerRunCmd += ' --name ' + currentInstance.containerName;
+        dockerRunCmd += ' -p ' + currentInstance.port + ':22';
+        dockerRunCmd += ' m2container ' + options.sshdCmd;
+        return dockerRunCmd;
+    };
 
-         if(RunningSshDaemons){
-            console.log("sshd is ready.");
-            next(false, instance);
-         } else {
-            console.log("sshd not ready yet.");
-            waitForSshd(next, instance, dockerContainerName);
-         }
-      });
+    var getNewInstance = function (next) {
+        var currentInstance = JSON.parse(JSON.stringify(options.instance));
+        options.instance.port++;
+        currentInstance.containerName = "m2Port" + currentInstance.port;
+        exec(constructDockerRunCommand(resources, currentInstance), function (error, stdout, stderr) {
+            if (error) {
+                var containerAlreadyStarted = error.message.match(/Conflict. The name/);
+                if (containerAlreadyStarted) {
+                    getNewInstance(next);
+                } else {
+                    console.error("There was an error starting the docker container: " + error.message);
+                    throw error;
+                }
+            } else {
+                waitForSshd(next, currentInstance);
+            }
+        });
+    };
+
+    waitForSshd = function (next, instance) {
+        var dockerRunningProcesses = "sudo docker exec " + instance.containerName + " ps aux";
+        var filterForSshd = "grep \"" + options.sshdCmd + "\"";
+        var excludeGrep = "grep -v grep";
+
+        exec(dockerRunningProcesses + " | " + filterForSshd + " | " + excludeGrep, function (error, stdout, stderr) {
+            if (error) {
+                console.error("There was an error while waiting for sshd: " + error);
+            }
+            var runningSshDaemons = stdout;
+
+            console.log("Looking for sshd. OUT: " + stdout + " ERR: " + stderr);
+
+            if (runningSshDaemons) {
+                console.log("sshd is ready.");
+                next(null, instance);
+            } else {
+                console.log("sshd not ready yet.");
+                waitForSshd(next, instance);
+            }
+        });
     };
 
     return {
-        getNewInstance : getNewInstance,
-        removeInstance : removeInstance,
-        updateLastActiveTime : function(instance){}
+        getNewInstance: getNewInstance,
+        removeInstance: removeInstance,
+        updateLastActiveTime: function (instance) {
+        }
     };
 
 };
 
-exports.manager = sudo_docker_manager;
+exports.manager = dockerManager;

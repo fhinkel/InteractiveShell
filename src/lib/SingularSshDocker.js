e04d691f78f2bec941fccb55b11da02169526504
Lars Kastner <lars.kastner@yahoo.dk>
2015-08-27 12:21:44 +0200

    Setting math program to singular.

--- src/lib/SingularSshDocker.js
+++ src/lib/SingularSshDocker.js
@@ -11,8 +11,8 @@ GLOBAL.OPTIONS = require('./default.js').get_config({
     },
     server_config: {
         port: 8002,
-        MATH_PROGRAM: 'Macaulay2',
-        MATH_PROGRAM_COMMAND: 'export WWWBROWSER=/usr/bin/open; export PATH=/usr/bin:$PATH; M2 --print-width 100',
+        MATH_PROGRAM: 'Singular',
+        MATH_PROGRAM_COMMAND: 'Singular',
         CONTAINERS: './sshDockerContainers.js'
     }
     guestInstance: {

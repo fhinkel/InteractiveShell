2f6325907ef82aca356d6f6cf1a8aab914d604e0
Lars Kastner <lars.kastner@yahoo.dk>
2015-12-01 17:54:08 +0100

    Default text files added.

---
+++ public/public-Singular/defaultText.js
@@ -0,0 +1,22 @@
+var DefaultText = "" +
+    "   /**\n" +
+    "    * Welcome to Singular online!\n" +
+    "    * In this window you may type in Singular commands \n" +
+    "    * and have them evaluated by the server.\n" +
+    "    * \n" +
+    "    * Evaluate a line or selection by typing Shift+Enter \n" +
+    "    * or by clicking on Evaluate.\n" +
+    "    * \n" +
+    "    * Here are some sample commands: */\n" +
+    "    **/\n" +
+    "  ring r = 101,(a,b,c,d),dp;\n" +
+    "  ideal I = a^2-b*c, a^3-b^3, a^4-b*d^3, a^5-c^2*d^3;\n" +
+    "  ideal G = groebner(I);\n" +
+    "\n" +
+    "  /*  Some examples of rings and orderings  */\n" +
+    "  ring r; // ZZ/32003ZZ[x,y,z]\n" +
+    "  ring s = 0,x(1..6),lp; // QQ[x(1)..x(6)]\n" +
+    "  ring R = (0,t),(x,y,z),dp; // QQ(t)[x,y,z]\n" +
+    "  ring C = (0,i),(x,y,z),dp; minpoly = i2+1;\n"
+    "  /******************************************/\n";
+

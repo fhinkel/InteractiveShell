78262876b5aa94cb0d426e75ae6dceb77b6feaec
Lars Kastner <power@velleda.mathematik.uni-kl.de>
2015-12-02 18:29:23 +0100

    Fixing json file.

--- src/lib/SingularSshDocker.js
+++ src/lib/SingularSshDocker.js
@@ -1,28 +1,28 @@
 GLOBAL.OPTIONS = require('./default.js').get_config({
     hostConfig: {
         dockerRunCmd: '',
-        maxContainerNumber: 10,
-        containerType: 'singular_container',
+        minContainerAge: 1000*3600*24*7,
+        maxContainerNumber: 30,
+        containerType: 'singular_container_power',
         sshdCmd: "/usr/sbin/sshd -D",
         host: 'localhost',
-        username: 'user',
+        username: 'power',
         port: '22',
-        sshKey: "/home/user/.ssh/id_rsa",
+        sshKey: "/home/power/.ssh/id_rsa",
         dockerCmdPrefix: ""
     },
     server_config: {
-        port: 8002,
+        port: 1337,
         MATH_PROGRAM: 'Singular',
         MATH_PROGRAM_COMMAND: 'Singular',
         CONTAINERS: './sshDockerContainers.js',
-	    CMD_LOG_FOLDER: '/home/user/cmd_logs',
         resumeString: 'Type \'listvar();\' to print the list of existing variables.\nType \'basering;\' to print the currently active ring.\n> '
     },
     guestInstance: {
         host: 'localhost',
         username: 'singularUser',
         port: '5000',
-        sshKey: "/home/user/InteractiveShell/Vagrant_singular/id_rsa",
+        sshKey: "/home/power/InteractiveShell/Vagrant_singular/id_rsa",
         containerName: 'singularContainer',
         lastActiveTime: 0
     },

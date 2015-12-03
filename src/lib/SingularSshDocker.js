13bfdce411ea5e5d50ce79368da848b14f5b6c1d
Lars Kastner <lars.kastner@yahoo.dk>
2015-12-03 17:52:00 +0100

    Limits for power user.

--- src/lib/SingularSshDocker.js
+++ src/lib/SingularSshDocker.js
@@ -1,8 +1,8 @@
 GLOBAL.OPTIONS = require('./default.js').get_config({
     hostConfig: {
         dockerRunCmd: '',
-        minContainerAge: 1000*3600*24*7,
-        maxContainerNumber: 30,
+        minContainerAge: 3600,
+        maxContainerNumber: 10,
         containerType: 'singular_container_power',
         sshdCmd: "/usr/sbin/sshd -D",
         host: 'localhost',
@@ -18,6 +18,10 @@ GLOBAL.OPTIONS = require('./default.js').get_config({
         CONTAINERS: './sshDockerContainers.js',
         resumeString: 'Type \'listvar();\' to print the list of existing variables.\nType \'basering;\' to print the currently active ring.\n> '
     },
+        per_container_resources: {
+            cpuShares: 4,
+            memory: 2048
+        },
     guestInstance: {
         host: 'localhost',
         username: 'singularUser',

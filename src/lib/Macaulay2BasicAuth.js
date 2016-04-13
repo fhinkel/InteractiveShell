490928e4ce20127d1c8e9f5d31f832ad9e15e929
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2016-04-13 18:15:22 -0400

    Minor code review changes

--- src/lib/Macaulay2BasicAuth.js
+++ src/lib/Macaulay2BasicAuth.js
@@ -1,13 +1,14 @@
 GLOBAL.OPTIONS = require('./default.js').get_config({
-    authentification: "basic",
+  authentification: "basic",
   server_config: {
     port: 8002,
-    MATH_PROGRAM_COMMAND: 'export WWWBROWSER=/usr/bin/open; export PATH=/usr/bin:$PATH; M2 --print-width 100',
+    MATH_PROGRAM_COMMAND: 'export WWWBROWSER=/usr/bin/open; ' +
+    'export PATH=/usr/bin:$PATH; M2 --print-width 100',
     CONTAINERS: './sudoDockerContainers.js'
   }
 });
 
-console.log(OPTIONS);
+console.log(GLOBAL.OPTIONS);
 
 var Macaulay2Server = require('./index.js').MathServer();
 Macaulay2Server.listen();
--- src/lib/index.js
+++ src/lib/index.js
@@ -3,23 +3,23 @@
 var express = require('express');
 var app = express();
 
-if(OPTIONS.authentification == "basic"){
-    var auth = require('http-auth');
-    var basic = auth.basic({
-        realm: "Please enter your username and password.",
-        file: __dirname + "/../../public/users.htpasswd" // gevorg:gpass, Sarah:testpass ... 
-    });
-    app.use(auth.connect(basic));
-    var getClientIdFromSocket = function(socket){
-        var clientId = socket.request.headers.authorization.substring(6);
-        return clientId;
-    }
+if (OPTIONS.authentification === "basic") {
+  var auth = require('http-auth');
+  var basic = auth.basic({
+    realm: "Please enter your username and password.",
+    file: __dirname + "/../../public/users.htpasswd"
+  });
+  app.use(auth.connect(basic));
+  var getClientIdFromSocket = function(socket) {
+    var clientId = socket.request.headers.authorization.substring(6);
+    return clientId;
+  };
 } else {
-    var getClientIdFromSocket = function(socket){
-        var cookies = socket.request.headers.cookie;
-        var clientId = cookies[OPTIONS.cookieName];
-        return clientId;
-    }
+  var getClientIdFromSocket = function(socket) {
+    var cookies = socket.request.headers.cookie;
+    var clientId = cookies[OPTIONS.cookieName];
+    return clientId;
+  };
 }
 
 var http = require('http').createServer(app);

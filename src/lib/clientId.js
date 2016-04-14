73b3fe72b839ac6b756c860aa3a525b46342e4e5
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2016-04-14 14:37:30 -0400

    Refactor clientId

--- src/lib/clientId.js
+++ src/lib/clientId.js
@@ -1,11 +1,12 @@
+var exists = function(clientId, clients, logFunction) {
+  if (clientId in clients) {
+    logFunction("Client already exists");
+    return true;
+  }
+  return false;
+};
+
 module.exports = function(clients, logExceptOnTest) {
-  var exists = function(clientId) {
-    if (clientId in clients) {
-      logExceptOnTest("Client already exists");
-      return true;
-    }
-    return false;
-  };
   return {
     getNewId: function() {
       var clientId;
@@ -13,7 +14,7 @@ module.exports = function(clients, logExceptOnTest) {
         var randomId = Math.random() * 1000000;
         randomId = Math.floor(randomId);
         clientId = "user" + randomId.toString(10);
-      } while (exists(clientId));
+      } while (exists(clientId, clients, logExceptOnTest));
       logExceptOnTest("New Client ID " + clientId);
       return clientId;
     }

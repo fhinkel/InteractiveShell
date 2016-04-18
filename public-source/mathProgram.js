d59bc61c5fcebdbcf7bfb3b6d3eff133e2cda471
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2016-04-17 21:53:46 -0400

    Add jquery-ui back in for highlight because of issue with touch devices and taps

--- public-source/mathProgram.js
+++ public-source/mathProgram.js
@@ -490,6 +490,9 @@ $(document).ready(function() {
   $(document).on("click", ".submenuItem", trym2.showLesson);
 
   var codeClickAction = function() {
+     $(this).effect("highlight", {
+     color: 'red'
+     }, 300);
     var code = $(this).text();
     code += "\n";
     $("#M2In").val($("#M2In").val() + code);

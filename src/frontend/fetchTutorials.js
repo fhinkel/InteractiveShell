28770adb8a68f18c6ba798d332ee7fddc9a65107
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2016-04-18 21:31:59 -0400

    Fix error about tutorials for pasword-restricted use

--- src/frontend/fetchTutorials.js
+++ src/frontend/fetchTutorials.js
@@ -1,14 +1,16 @@
 /* global fetch */
 module.exports = function(callback) {
   console.log("Fetch tutorials.");
-  fetch('/getListOfTutorials')
+  fetch('/getListOfTutorials', {
+    credentials: 'same-origin'
+  })
       .then(function(data) {
         return data.json();
       }).then(function(tutorialPaths) {
         console.log("Obtaining list of tutorials successful: " + tutorialPaths);
         callback(0, tutorialPaths);
       }).catch(function(error) {
-        console.log("There was an error obtaining the list of tutorial files: " +
-        error);
-      });
+    console.log("There was an error obtaining the list of " +
+        "tutorial files: " + error);
+  });
 };

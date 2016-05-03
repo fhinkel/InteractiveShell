1bb4fd8c7a84b75e975ac301f558e711f850f3a4
Lars Kastner <lars.kastner@yahoo.dk>
2016-05-03 18:36:12 +0200

    Missing comma

--- src/startupConfigs/SingularSudoDocker.js
+++ src/startupConfigs/SingularSudoDocker.js
@@ -3,7 +3,7 @@ require('./startup.js')({
     port: 8002,
     MATH_PROGRAM: 'Singular',
     MATH_PROGRAM_COMMAND: 'Singular',
-    CONTAINERS: '../lib/sudoDockerContainers.js'
+    CONTAINERS: '../lib/sudoDockerContainers.js',
     resumeString: 'Type \'listvar();\' to print the list of ' +
     'existing variables.\n' +
     'Type \'basering;\' to print the currently active ring.\n> '

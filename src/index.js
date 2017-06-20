a7674d3827d6927be149d16d0de98d02ef0b6ecc
Lars Kastner <lars.kastner@yahoo.dk>
2017-06-20 20:08:18 +0200

    Created index.js for parsing command line arguments

---
+++ src/index.js
@@ -0,0 +1,41 @@
+var mathProgram = "Macaulay2";
+var mode = "docker";
+var args = process.argv;
+var n = args.length;
+
+if (n > 2) {
+  mathProgram = args[2];
+}
+
+if (n > 3) {
+  mode = args[3];
+}
+
+if (n > 4) {
+  console.log("Too many options");
+  process.exit(0);
+}
+
+if (mathProgram === "Macaulay2" || mathProgram === "M2") {
+  if (mode === "local") {
+      require("./startupConfigs/Macaulay2LocalServer.js");
+    } else if (mode === "docker") {
+      require("./startupConfigs/Macaulay2SudoDocker.js");
+    } else if (mode === "ssh") {
+      require("./startupConfigs/Macaulay2SshDocker.js");
+    } else {
+      console.log("There is no mode " + mode);
+    }
+} else if (mathProgram === "Singular") {
+  if (mode === "local") {
+      require("./startupConfigs/SingularLocalServer.js");
+    } else if (mode === "docker") {
+      require("./startupConfigs/SingularSudoDocker.js");
+    } else if (mode === "ssh") {
+      require("./startupConfigs/SingularSshDocker.js");
+    } else {
+      console.log("There is no mode " + mode);
+    }
+} else {
+  console.log("Did not recognize math program " + mathProgram);
+}

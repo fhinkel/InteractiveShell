d9fa59b26565b1a52ec5c4d51cdfeeaf187920ba
Franziska Hinkelmann <franzih@chromium.org>
2017-06-22 15:51:48 -0400

    Delete fetch polyfill.

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    }),
     new webpack.optimize.UglifyJsPlugin({minimize: true})
   ]
 };
--- webpack.debug-config.js
+++ webpack.debug-config.js
@@ -12,8 +12,5 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    })
   ]
 };

6bcd909b42a96c7a543fa7548b8585f6a6ff8d12
Lars Kastner <lars.kastner@yahoo.dk>
2019-02-26 12:02:22 +0100

    Some autogenerated stuff changed after bumping versions

--- public/public-common/mathJaxConfig.js
+++ public/public-common/mathJaxConfig.js
@@ -4,13 +4,13 @@ MathJax.Hub.Config({tex2jax: {inlineMath: [['$', '$'], ['\\(', '\\)']]}});
 MathJax.Hub.Config({
   TeX: {
     Macros: {
-      PP: "{\\mathbb{P}}",
-      ZZ: "{\\mathbb{Z}}",
-      QQ: "{\\mathbb{Q}}",
-      RR: "{\\mathbb{R}}",
-      CC: "{\\mathbb{C}}",
-      mac: "{{\\it Macaulay2}}",
-      bold: ["{\\bf #1}", 1]
-    }
-  }
+      PP: '{\\mathbb{P}}',
+      ZZ: '{\\mathbb{Z}}',
+      QQ: '{\\mathbb{Q}}',
+      RR: '{\\mathbb{R}}',
+      CC: '{\\mathbb{C}}',
+      mac: '{{\\it Macaulay2}}',
+      bold: ['{\\bf #1}', 1],
+    },
+  },
 });

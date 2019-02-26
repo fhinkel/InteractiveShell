3eb58286d85d5203f2ea45e9905fc1745a9b213e
Lars Kastner <lars.kastner@yahoo.dk>
2019-02-26 10:44:03 +0100

    Bump webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,10 @@ module.exports = {
     path: __dirname,
     filename: "public/public-common/index.js"
   },
+  mode: 'production',
   module: {
-    loaders: [
-      {test: /\.css$/, loader: "style!css"}
-    ]
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({minimize: true, sourceMap: true})
   ],
   devtool: "source-map"
 };
